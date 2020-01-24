10a83956a8bf32192c4583c3bff82e0f88de6d92
psi <ryo.hirafuji@link-u.co.jp>
2020-01-25 07:00:16 +0900

    from YUV to RGB

--- src/avif/img/Conversion.hpp
+++ src/avif/img/Conversion.hpp
@@ -6,6 +6,7 @@
 
 #include <cstdint>
 #include <cmath>
+#include <tuple>
 #include "Image.hpp"
 
 namespace avif::img {
@@ -18,29 +19,33 @@ namespace spec {
 template <uint8_t bitsPerComponent> struct YUV;
 template <> struct YUV<8> {
   using Type = uint8_t;
+  static constexpr uint8_t bias = 128;
 };
 template <> struct YUV<10> {
   using Type = uint16_t;
+  static constexpr uint16_t bias = 512;
 };
 template <> struct YUV<12> {
   using Type = uint16_t;
+  static constexpr uint16_t bias = 2048;
 };
 
 }
 
 namespace detail{
 
-constexpr int clamp(int value, int low, int high) {
+template <typename T>
+constexpr T clamp(T value, T low, T high) {
   return value < low ? low : (value > high ? high : value);
 }
 
 template <size_t rgbBits, size_t yuvBits>
-constexpr void calcYUV(uint16_t const sr, uint16_t const sg, uint16_t const sb, typename spec::YUV<yuvBits>::Type* dstY, typename spec::YUV<yuvBits>::Type* dstU, typename spec::YUV<yuvBits>::Type* dstV) {
+constexpr void calcYUV(uint16_t const ir, uint16_t const ig, uint16_t const ib, typename spec::YUV<yuvBits>::Type* dstY, typename spec::YUV<yuvBits>::Type* dstU, typename spec::YUV<yuvBits>::Type* dstV) {
   using YUVType = typename spec::YUV<yuvBits>::Type;
   using RGBType = typename spec::RGB<rgbBits>::Type;
-  float const r = static_cast<float>(sr) / spec::RGB<rgbBits>::max;
-  float const g = static_cast<float>(sg) / spec::RGB<rgbBits>::max;
-  float const b = static_cast<float>(sb) / spec::RGB<rgbBits>::max;
+  float const r = static_cast<float>(ir) / spec::RGB<rgbBits>::max;
+  float const g = static_cast<float>(ig) / spec::RGB<rgbBits>::max;
+  float const b = static_cast<float>(ib) / spec::RGB<rgbBits>::max;
 
   auto constexpr shift = static_cast<float>(1u << (yuvBits - 8u));
   int constexpr maxYUV = (1u << yuvBits) - 1u;
@@ -57,8 +62,28 @@ constexpr void calcYUV(uint16_t const sr, uint16_t const sg, uint16_t const sb, 
   }
 }
 
+template <size_t rgbBits, size_t yuvBits>
+constexpr std::tuple<typename spec::RGB<rgbBits>::Type, typename spec::RGB<rgbBits>::Type, typename spec::RGB<rgbBits>::Type> calcRGB(typename spec::YUV<yuvBits>::Type const* srcY, typename spec::YUV<yuvBits>::Type const* srcU, typename spec::YUV<yuvBits>::Type const* srcV) {
+  using YUVType = typename spec::YUV<yuvBits>::Type;
+  using RGBType = typename spec::RGB<rgbBits>::Type;
+
+  auto constexpr shift = static_cast<float>(1u << (yuvBits - 8u));
+  auto const y = ((static_cast<float>(*srcY) / shift) - 128.f) / 219.0f;
+  auto const u = ((static_cast<float>(srcU != nullptr ? (*srcU) : spec::YUV<yuvBits>::bias) / shift) - 128.0f) / 224.0f;
+  auto const v = ((static_cast<float>(srcV != nullptr ? (*srcV) : spec::YUV<yuvBits>::bias) / shift) - 128.0f) / 224.0f;
+
+  float const r = y                    + (+1.47460f) * v;
+  float const g = y + (-0.16455f) * u  + (-0.57135f) * v;
+  float const b = y + (+1.88140f) * u;
+
+  auto const ir = static_cast<RGBType>(clamp<int>(static_cast<int>(r * spec::RGB<rgbBits>::max), 0, spec::RGB<rgbBits>::max));
+  auto const ig = static_cast<RGBType>(clamp<int>(static_cast<int>(g * spec::RGB<rgbBits>::max), 0, spec::RGB<rgbBits>::max));
+  auto const ib = static_cast<RGBType>(clamp<int>(static_cast<int>(b * spec::RGB<rgbBits>::max), 0, spec::RGB<rgbBits>::max));
+  return std::make_tuple(ir, ig, ib);
+}
+
 template <uint8_t rgbBits, uint8_t yuvBits, bool subX, bool subY>
-void constexpr convert(size_t width, size_t height, uint8_t bytesPerPixel, uint8_t const* src, size_t stride, uint8_t* dstY, size_t strideY, uint8_t* dstU, size_t strideU, uint8_t* dstV, size_t strideV) {
+void constexpr convertFromRGB(size_t width, size_t height, uint8_t bytesPerPixel, uint8_t const* src, size_t stride, uint8_t* dstY, size_t strideY, uint8_t* dstU, size_t strideU, uint8_t* dstV, size_t strideV) {
   using YUVType = typename spec::YUV<yuvBits>::Type;
   using RGBType = typename spec::RGB<rgbBits>::Type;
   uint8_t* lineY = dstY;
@@ -99,19 +124,74 @@ void constexpr convert(size_t width, size_t height, uint8_t bytesPerPixel, uint8
   }
 }
 
+template <uint8_t rgbBits, uint8_t yuvBits, bool subX, bool subY>
+void constexpr convertFromYUV(size_t width, size_t height, uint8_t bytesPerPixel, uint8_t* dst, size_t stride, uint8_t const* srcY, size_t strideY, uint8_t const* srcU, size_t strideU, uint8_t const* srcV, size_t strideV) {
+  using YUVType = typename spec::YUV<yuvBits>::Type;
+  using RGBType = typename spec::RGB<rgbBits>::Type;
+  uint8_t const* lineY = srcY;
+  uint8_t const* lineU = srcU;
+  uint8_t const* lineV = srcV;
+  uint8_t* line = dst;
+  for(size_t y = 0; y < height; ++y) {
+    uint8_t* ptr = line;
+    auto const* ptrY = reinterpret_cast<YUVType const*>(lineY);
+    auto const* ptrU = reinterpret_cast<YUVType const*>(lineU);
+    auto const* ptrV = reinterpret_cast<YUVType const*>(lineV);
+    for (size_t x = 0; x < width; ++x) {
+      RGBType& r = reinterpret_cast<RGBType*>(ptr)[0];
+      RGBType& g = reinterpret_cast<RGBType*>(ptr)[1];
+      RGBType& b = reinterpret_cast<RGBType*>(ptr)[2];
+      if (subX) {
+        if(x % 2 == 0) {
+          std::tie(r,g,b) = calcRGB<rgbBits, yuvBits>(&ptrY[x], &ptrU[x/2], &ptrV[x/2]);
+        } else {
+          std::tie(r,g,b) = calcRGB<rgbBits, yuvBits>(&ptrY[x], nullptr, nullptr);
+        }
+      } else {
+        std::tie(r,g,b) = calcRGB<rgbBits, yuvBits>(&ptrY[x], &ptrU[x], &ptrV[x]);
+      }
+      ptr += bytesPerPixel;
+    }
+    lineY += strideY;
+    if(subY){
+      if(y % 2 == 1) {
+        lineU += strideU;
+        lineV += strideV;
+      }
+    } else {
+      lineU += strideU;
+      lineV += strideV;
+    }
+    line += stride;
+  }
+}
+
 }
 
 
 template <uint8_t rgbBits, uint8_t yuvBits>
 struct FromRGB final {
   void toI444(Image<rgbBits> const& src, uint8_t* dstY, size_t strideY, uint8_t* dstU, size_t strideU, uint8_t* dstV, size_t strideV) {
-    detail::convert<rgbBits, yuvBits, false, false>(src.width(), src.height(), src.bytesPerPixel(), src.data(), src.stride(), dstY, strideY, dstU, strideU, dstV, strideV);
+    detail::convertFromRGB<rgbBits, yuvBits, false, false>(src.width(), src.height(), src.bytesPerPixel(), src.data(), src.stride(), dstY, strideY, dstU, strideU, dstV, strideV);
   }
   void toI422(Image<rgbBits> const& src, uint8_t* dstY, size_t strideY, uint8_t* dstU, size_t strideU, uint8_t* dstV, size_t strideV){
-    detail::convert<rgbBits, yuvBits, true, false>(src.width(), src.height(), src.bytesPerPixel(), src.data(), src.stride(), dstY, strideY, dstU, strideU, dstV, strideV);
+    detail::convertFromRGB<rgbBits, yuvBits, true, false>(src.width(), src.height(), src.bytesPerPixel(), src.data(), src.stride(), dstY, strideY, dstU, strideU, dstV, strideV);
   }
   void toI420(Image<rgbBits> const& src, uint8_t* dstY, size_t strideY, uint8_t* dstU, size_t strideU, uint8_t* dstV, size_t strideV){
-    detail::convert<rgbBits, yuvBits, true, true>(src.width(), src.height(), src.bytesPerPixel(), src.data(), src.stride(), dstY, strideY, dstU, strideU, dstV, strideV);
+    detail::convertFromRGB<rgbBits, yuvBits, true, true>(src.width(), src.height(), src.bytesPerPixel(), src.data(), src.stride(), dstY, strideY, dstU, strideU, dstV, strideV);
+  }
+};
+
+template <uint8_t rgbBits, uint8_t yuvBits>
+struct ToRGB final {
+  void fromI444(Image<rgbBits>& dst, uint8_t* srcY, size_t strideY, uint8_t* srcU, size_t strideU, uint8_t* srcV, size_t strideV) {
+    detail::convertFromYUV<rgbBits, yuvBits, false, false>(dst.width(), dst.height(), dst.bytesPerPixel(), dst.data(), dst.stride(), srcY, strideY, srcU, strideU, srcV, strideV);
+  }
+  void fromI422(Image<rgbBits>& dst, uint8_t* srcY, size_t strideY, uint8_t* srcU, size_t strideU, uint8_t* srcV, size_t strideV){
+    detail::convertFromYUV<rgbBits, yuvBits, true, false>(dst.width(), dst.height(), dst.bytesPerPixel(), dst.data(), dst.stride(), srcY, strideY, srcU, strideU, srcV, strideV);
+  }
+  void fromI420(Image<rgbBits>& dst, uint8_t* srcY, size_t strideY, uint8_t* srcU, size_t strideU, uint8_t* srcV, size_t strideV){
+    detail::convertFromYUV<rgbBits, yuvBits, true, true>(dst.width(), dst.height(), dst.bytesPerPixel(), dst.data(), dst.stride(), srcY, strideY, srcU, strideU, srcV, strideV);
   }
 };
 
