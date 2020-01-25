f41b44e37965797d35eed22e125155787fbf0abd
psi <ryo.hirafuji@link-u.co.jp>
2020-01-25 09:46:27 +0900

    separate header

--- src/avif/img/Transform.hpp
+++ src/avif/img/Transform.hpp
@@ -8,93 +8,9 @@
 #include "Image.hpp"
 #include "../ImageMirrorBox.hpp"
 #include "../ImageRotationBox.hpp"
+#include "TransformImpl.hpp"
 
 namespace avif::img {
-namespace transform {
-
-template <ImageMirrorBox::Axis>
- struct FlipTrans;
-template <>
-struct FlipTrans<ImageMirrorBox::Axis::Vertical> {
-  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
-    return std::make_tuple(x, srcHeight - y);
-  }
-};
-
-template <>
-struct FlipTrans<ImageMirrorBox::Axis::Horizontal> {
-  std::tuple <size_t,size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
-    return std::make_tuple(srcWidth - x, y);
-  }
-};
-
-template <ImageRotationBox::Rotation>
-struct RotateTrans;
-template <>
-struct RotateTrans<ImageRotationBox::Rotation::Rot0> {
-  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
-    return std::make_tuple(x, y);
-  }
-};
-
-template <>
-struct RotateTrans<ImageRotationBox::Rotation::Rot90> {
-  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
-    return std::make_tuple(y, x);
-  }
-};
-
-template <>
-struct RotateTrans<ImageRotationBox::Rotation::Rot180> {
-  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
-    return std::make_tuple(srcWidth - x, srcHeight - y);
-  }
-};
-
-template <>
-struct RotateTrans<ImageRotationBox::Rotation::Rot270> {
-  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
-    return std::make_tuple(y, srcHeight - x);
-  }
-};
-
-template < size_t BitsPerComponent, typename Trans >
-void fill(Image <BitsPerComponent> const& src, Image <BitsPerComponent>& dst) {
-  Trans trans;
-  using PixelType = typename spec::RGB <BitsPerComponent>::Type;
-
-  // src
-  uint8_t const* srcBase = src.data();
-  size_t const srcStride = src.stride();
-  size_t const srcBytesPerPixel = dst.bytesPerPixel();
-  size_t const srcWidth = src.width();
-  size_t const srcHeight = src.height();
-
-  // dst
-  uint8_t* dstLine = dst.data();
-  size_t const dstStride = dst.stride();
-  size_t const dstBytesPerPixel = dst.bytesPerPixel();
-  size_t const dstWidth = dst.width();
-  size_t const dstHeight = dst.height();
-
-  // common
-  size_t const components = src.numComponents();
-  for (size_t y = 0; y < dstHeight; ++y) {
-    auto * dstPtr = reinterpret_cast<PixelType*>(dstLine);
-    for (size_t x = 0; x < dstWidth; ++x) {
-      auto * dstPixel = reinterpret_cast<PixelType*>(dstPtr);
-      auto from = trans(srcWidth, x, srcHeight, y);
-      auto const * srcPixel = reinterpret_cast<PixelType const*>(srcBase + (srcStride * std::get<1>(from)) + (srcBytesPerPixel * std::get<0>(from)));
-      for (size_t c = 0; c < components; ++c) {
-        dstPixel[c] = srcPixel[c];
-      }
-      dstPtr += dstBytesPerPixel;
-    }
-    dstLine += dstStride;
-  }
-}
-
-}
 
 template <size_t BitsPerComponent>
 Image <BitsPerComponent> flip(Image <BitsPerComponent> const & src, ImageMirrorBox::Axis const axis) {
@@ -114,7 +30,7 @@ Image <BitsPerComponent> flip(Image <BitsPerComponent> const & src, ImageMirrorB
 }
 
 template <size_t BitsPerComponent>
-Image <BitsPerComponent> rotate(Image < BitsPerComponent > const & src, ImageRotationBox::Rotation const rotation) {
+Image <BitsPerComponent> rotate(Image<BitsPerComponent> const & src, ImageRotationBox::Rotation const rotation) {
   Image<BitsPerComponent> dst;
   switch (rotation) {
   case ImageRotationBox::Rotation::Rot0: {
---
+++ src/avif/img/TransformImpl.hpp
@@ -0,0 +1,96 @@
+//
+// Created by psi on 2020/01/25.
+//
+
+#pragma once
+
+#include "Image.hpp"
+#include "../ImageMirrorBox.hpp"
+#include "../ImageRotationBox.hpp"
+
+namespace avif::img::transform {
+
+template <ImageMirrorBox::Axis>
+ struct FlipTrans;
+template <>
+struct FlipTrans<ImageMirrorBox::Axis::Vertical> {
+  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
+    return std::make_tuple(x, srcHeight - y);
+  }
+};
+
+template <>
+struct FlipTrans<ImageMirrorBox::Axis::Horizontal> {
+  std::tuple <size_t,size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
+    return std::make_tuple(srcWidth - x, y);
+  }
+};
+
+template <ImageRotationBox::Rotation>
+struct RotateTrans;
+template <>
+struct RotateTrans<ImageRotationBox::Rotation::Rot0> {
+  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
+    return std::make_tuple(x, y);
+  }
+};
+
+template <>
+struct RotateTrans<ImageRotationBox::Rotation::Rot90> {
+  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
+    return std::make_tuple(y, x);
+  }
+};
+
+template <>
+struct RotateTrans<ImageRotationBox::Rotation::Rot180> {
+  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
+    return std::make_tuple(srcWidth - x, srcHeight - y);
+  }
+};
+
+template <>
+struct RotateTrans<ImageRotationBox::Rotation::Rot270> {
+  std::tuple <size_t, size_t> operator()(size_t srcWidth, size_t x, size_t srcHeight, size_t y) {
+    return std::make_tuple(y, srcHeight - x);
+  }
+};
+
+template < size_t BitsPerComponent, typename Trans >
+void fill(Image<BitsPerComponent> const& src, Image<BitsPerComponent>& dst) {
+  Trans trans;
+
+  using PixelType = typename spec::RGB <BitsPerComponent>::Type;
+
+  // src
+  uint8_t const* srcBase = src.data();
+  size_t const srcStride = src.stride();
+  size_t const srcBytesPerPixel = dst.bytesPerPixel();
+  size_t const srcWidth = src.width();
+  size_t const srcHeight = src.height();
+
+  // dst
+  uint8_t* dstLine = dst.data();
+  size_t const dstStride = dst.stride();
+  size_t const dstBytesPerPixel = dst.bytesPerPixel();
+  size_t const dstWidth = dst.width();
+  size_t const dstHeight = dst.height();
+
+  // common
+  size_t const components = src.numComponents();
+  for (size_t y = 0; y < dstHeight; ++y) {
+    auto * dstPtr = reinterpret_cast<PixelType*>(dstLine);
+    for (size_t x = 0; x < dstWidth; ++x) {
+      auto * dstPixel = reinterpret_cast<PixelType*>(dstPtr);
+      auto from = trans(srcWidth, x, srcHeight, y);
+      auto const * srcPixel = reinterpret_cast<PixelType const*>(srcBase + (srcStride * std::get<1>(from)) + (srcBytesPerPixel * std::get<0>(from)));
+      for (size_t c = 0; c < components; ++c) {
+        dstPixel[c] = srcPixel[c];
+      }
+      dstPtr += dstBytesPerPixel;
+    }
+    dstLine += dstStride;
+  }
+}
+
+}
