c12886495811d0e5681ea4c2a0537b3befb821c5
psi <ryo.hirafuji@link-u.co.jp>
2020-02-08 03:25:12 +0900

    pass test

--- test/ColorTest.cpp
+++ test/ColorTest.cpp
@@ -7,16 +7,31 @@
 #include <gtest/gtest.h>
 #include "../src/avif/img/Conversion.hpp"
 
-TEST(ColorTest, RedTest) {
+TEST(ColorTest, RedTestLimited) {
   using namespace avif::img;
   uint8_t y = 0;
   uint8_t u = 0;
   uint8_t v = 0;
-  detail::calcYUV<8,8>(255, 0, 0, &y, &u, &v);
+  detail::calcYUV<8,8, false>(255, 0, 0, &y, &u, &v);
   uint8_t r = 0;
   uint8_t g = 0;
   uint8_t b = 0;
-  std::tie(r,g,b) = detail::calcRGB<8,8>(&y, &u, &v);
+  std::tie(r,g,b) = detail::calcRGB<8,8, false>(&y, &u, &v);
+  ASSERT_TRUE(254 <= r);
+  ASSERT_TRUE(g <= 1);
+  ASSERT_TRUE(b <= 1);
+}
+
+TEST(ColorTest, RedTestFull) {
+  using namespace avif::img;
+  uint8_t y = 0;
+  uint8_t u = 0;
+  uint8_t v = 0;
+  detail::calcYUV<8,8, true>(255, 0, 0, &y, &u, &v);
+  uint8_t r = 0;
+  uint8_t g = 0;
+  uint8_t b = 0;
+  std::tie(r,g,b) = detail::calcRGB<8,8, true>(&y, &u, &v);
   ASSERT_TRUE(254 <= r);
   ASSERT_TRUE(g <= 1);
   ASSERT_TRUE(b <= 1);
