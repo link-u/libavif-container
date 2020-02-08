b436fc9b5b36d8b5f5999be0f5463642ca13d0d0
psi <ryo.hirafuji@link-u.co.jp>
2020-02-08 14:14:03 +0900

    add unit test for YUV conversions

--- test/ColorTest.cpp
+++ test/ColorTest.cpp
@@ -7,32 +7,54 @@
 #include <gtest/gtest.h>
 #include "../src/avif/img/Conversion.hpp"
 
-TEST(ColorTest, RedTestLimited) {
+TEST(ColorTest, LimitedTest) {
   using namespace avif::img;
-  uint8_t y = 0;
-  uint8_t u = 0;
-  uint8_t v = 0;
-  detail::calcYUV<8,8, false>(255, 0, 0, &y, &u, &v);
-  uint8_t r = 0;
-  uint8_t g = 0;
-  uint8_t b = 0;
-  std::tie(r,g,b) = detail::calcRGB<8,8, false>(&y, &u, &v);
-  ASSERT_TRUE(254 <= r);
-  ASSERT_TRUE(g <= 1);
-  ASSERT_TRUE(b <= 1);
+  { // Red color
+    uint16_t y = 0;
+    uint16_t u = 0;
+    uint16_t v = 0;
+    detail::calcYUV<8, 12, false>(255, 0, 0, &y, &u, &v);
+    uint8_t r = 0;
+    uint8_t g = 0;
+    uint8_t b = 0;
+    std::tie(r,g,b) = detail::calcRGB<8, 12, false>(&y, &u, &v);
+    ASSERT_EQ(255, r);
+    ASSERT_EQ(0, g);
+    ASSERT_EQ(0, b);
+  }
+  { // White must have max luma and no chroma.
+    uint8_t y = 0;
+    uint8_t u = 0;
+    uint8_t v = 0;
+    detail::calcYUV<8, 8, false>(255, 255, 255, &y, &u, &v);
+    ASSERT_EQ(235, y);
+    ASSERT_EQ(128, u);
+    ASSERT_EQ(128, v);
+  }
 }
 
-TEST(ColorTest, RedTestFull) {
+TEST(ColorTest, FullTest) {
   using namespace avif::img;
-  uint8_t y = 0;
-  uint8_t u = 0;
-  uint8_t v = 0;
-  detail::calcYUV<8,8, true>(255, 0, 0, &y, &u, &v);
-  uint8_t r = 0;
-  uint8_t g = 0;
-  uint8_t b = 0;
-  std::tie(r,g,b) = detail::calcRGB<8,8, true>(&y, &u, &v);
-  ASSERT_TRUE(254 <= r);
-  ASSERT_TRUE(g <= 1);
-  ASSERT_TRUE(b <= 1);
+  { // Red color
+    uint16_t y = 0;
+    uint16_t u = 0;
+    uint16_t v = 0;
+    detail::calcYUV<8, 12, true>(255, 0, 0, &y, &u, &v);
+    uint8_t r = 0;
+    uint8_t g = 0;
+    uint8_t b = 0;
+    std::tie(r,g,b) = detail::calcRGB<8, 12, true>(&y, &u, &v);
+    ASSERT_EQ(255, r);
+    ASSERT_EQ(0, g);
+    ASSERT_EQ(0, b);
+  }
+  { // White must have max luma and no chroma.
+    uint8_t y = 0;
+    uint8_t u = 0;
+    uint8_t v = 0;
+    detail::calcYUV<8, 8, true>(255, 255, 255, &y, &u, &v);
+    ASSERT_EQ(255, y);
+    ASSERT_EQ(128, u);
+    ASSERT_EQ(128, v);
+  }
 }
