c3b08194aab1ff1390854abd5cbbc7dbd4a02c4d
psi <ryo.hirafuji@link-u.co.jp>
2020-02-25 04:35:58 +0900

    add Constants

---
+++ src/avif/Constants.cpp
@@ -0,0 +1,13 @@
+//
+// Created by psi on 2020/02/25.
+//
+
+#pragma once
+
+#include "Constants.hpp"
+
+namespace avif {
+
+const std::string kAlphaAuxType = "urn:mpeg:mpegB:cicp:systems:auxiliary:alpha";
+
+}
---
+++ src/avif/Constants.hpp
@@ -0,0 +1,13 @@
+//
+// Created by psi on 2020/02/25.
+//
+
+#pragma once
+
+#include <string>
+
+namespace avif {
+
+extern const std::string kAlphaAuxType;
+
+}
