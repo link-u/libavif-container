ea09330be2bd15871ae702186a747797b05463a3
psi <ryo.hirafuji@link-u.co.jp>
2022-03-24 13:41:53 +0900

    add default value from Amd_2_2021

--- src/avif/ColourInformationBox.hpp
+++ src/avif/ColourInformationBox.hpp
@@ -7,11 +7,20 @@
 namespace avif {
 
 struct ColourInformationBox final : public Box {
-  struct CICP {
+  struct CICP final {
     uint16_t colourPrimaries{};
     uint16_t transferCharacteristics{};
     uint16_t matrixCoefficients{};
     bool fullRangeFlag{};
+    // See ISO_IEC_23000-22_2019_Amd_2_2021(en)
+    // p.5
+    CICP()
+    :colourPrimaries(1)
+    ,transferCharacteristics(13)
+    ,matrixCoefficients(5 /* or 6 */)
+    ,fullRangeFlag(true)
+    {
+    }
   };
   struct RestrictedICC {
     std::vector<uint8_t> payload;
