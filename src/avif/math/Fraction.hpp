1d3cf916b6b3e4c54fd751986336d6ac99f186fb
psi <ryo.hirafuji@link-u.co.jp>
2022-04-03 12:06:53 +0900

    Care about minus division

--- src/avif/math/Fraction.hpp
+++ src/avif/math/Fraction.hpp
@@ -14,31 +14,36 @@ private:
   int32_t numerator_;
   int32_t denominator_;
 public:
-  explicit Fraction(int32_t const numerator, int32_t const denominator)
+  explicit constexpr Fraction(int32_t const numerator, int32_t const denominator)
   :numerator_(denominator >= 0 ? numerator : -numerator)
   ,denominator_(std::abs(denominator))
   {
   }
 
-  [[ nodiscard ]] Fraction reduce() const {
-    int32_t r = std::gcd(std::abs(numerator_), std::abs(denominator_));
+  [[ nodiscard ]] constexpr Fraction reduce() const {
+    if(denominator_ == 0) {
+      return Fraction(0, 1);
+    }
+    int32_t const r = std::gcd(std::abs(numerator_), std::abs(denominator_));
     return Fraction(numerator_ / r, denominator_ / r);
   }
-  [[ nodiscard ]] Fraction minus(Fraction const& b) const {
+  [[ nodiscard ]] constexpr Fraction minus(Fraction const& b) const {
     int32_t const r = std::lcm(std::abs(denominator_), std::abs(b.denominator_));
     return Fraction((r / denominator_ * numerator_) - (r / b.denominator_ * b.numerator_), r).reduce();
   }
-  [[ nodiscard ]] int32_t numerator() const {
+  [[ nodiscard ]] constexpr int32_t numerator() const {
     return numerator_;
   }
-  [[ nodiscard ]]int32_t denominator() const {
+  [[ nodiscard ]] constexpr int32_t denominator() const {
     return denominator_;
   }
-  [[ nodiscard ]] bool isInteger() const {
+  [[ nodiscard ]] constexpr bool isInteger() const {
     return denominator_ == 1;
   }
-  [[ nodiscard ]] Fraction div(int32_t const d) const {
-    return Fraction(numerator_, denominator_ * d).reduce();
+  [[ nodiscard ]] constexpr Fraction div(int32_t const d) const {
+    return d > 0 ?
+      Fraction(numerator_, denominator_ * d).reduce() :
+      Fraction(-numerator_, denominator_ * -d).reduce();
   }
 };
 
